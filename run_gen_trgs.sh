5e5af6c0f54a9e7f9a016e93d1a00b1fef9d7bf5
Blake Jackson <blake.jackson@rtslabs.com>
2016-01-15 17:21:55 -0500

    first commit

---
+++ run_gen_trgs.sh
@@ -0,0 +1,20 @@
+sqlplus tiger/kigertiger@"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.20.0.191)(PORT=1521))(CONNECT_DATA=(SERVER=dedicated)(SID=wwdev)))"
+
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql CARRIER_CUSTOMER
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql CUSTOMER
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql CUSTOMER_ORDER
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql EDI_FILE
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql IB_INVOICE
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql LN_ITM_CHARGE
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql LN_ITM_SUMMARY
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql OB_CUST_INVOICE
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql OB_CUST_INVOICE_INFO
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql PACKAGE
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql PARTY
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/GENERATE_AUDIT_TRIGGER.sql SHIPMENT_INFO
+
+
+
+@/Users/macuser/Documents/DiscoverIT/RTS/WWEX-Audit/DeliveryToRTS/script-output/gentrgs.sql
+
+
